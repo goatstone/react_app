9eeee53208091f3ea17dd49d442770a6d05e08d2
JoseHerminioCollas <JoseHerminioCollas@goatstone.com>
2015-07-10 16:11:08 -0700

    Add Radium and component Spinner.

--- src/goatstone/main.js
+++ src/goatstone/main.js
@@ -1,4 +1,7 @@
 /** @jsx React.DOM */
 var React = require('react');
 var App = require('./App.js');
+var Spinner = require('./ui/Spinner.js');
+
 React.render(<App/>, document.body);
+React.render(<Spinner/>, document.querySelector('div div')); 
---
+++ src/goatstone/ui/Spinner.js
@@ -0,0 +1,38 @@
+/* Spinner.js Jose Collas : 7.2015 */
+var React = require('react');
+var Radium = require('Radium');
+var Spinner = React.createClass({
+  render: function () {
+    return (
+      <div style={spinnerStyles.inner}>{String.fromCharCode(8634)}  </div>
+    );
+  }
+});
+Spinner = Radium(Spinner);
+// pulse key frames
+var rotatePKF = Radium.keyframes({
+  '0%': {
+    transform: 'rotateY(0deg)' 
+  },
+  '100%': {
+    transform: 'rotateY(360deg)' 
+  },
+}); 
+var spinnerStyles = {
+  inner: {
+  	position:'absolute',
+    textAlign:'center',
+    top: '40%',
+    left: '40%',
+    background: 'rgba(100,100,255, 0.5)',
+    borderRadius: '50px',
+    height: '100px',
+    width: '100px',
+    fontSize: '112px',
+    animation: rotatePKF + ' 5s linear 0s infinite alternate both', 
+    boxShadow: '19px 16px 27px rgba(20, 110, 200, 0.5)',
+    color: 'orange',
+    padding: '32px 10px 20px 10px' 
+  }
+};
+module.exports = Spinner;
