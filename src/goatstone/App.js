0135f410cc52e02488da3f77893eed8c27a0919a
JoseHerminioCollas <JoseHerminioCollas@goatstone.com>
2015-07-13 14:04:54 -0700

    Adding component Bouncer, modify App.js.

--- src/goatstone/App.js
+++ src/goatstone/App.js
@@ -1,17 +1,18 @@
 /** @jsx React.DOM */
 var React = require('react');
-var Radium = require('Radium');
+var Radium = require('radium');
+var Spinner = require('./ui/Spinner.js');
+var Bouncer = require('./ui/Bouncer.js');
 
 var App = React.createClass({
 	render: function() {
 		return (
-			<div> 
-				<div>application</div>
+			<div>  
+				<h2>React Radium</h2>
+				<Spinner />			
+ 				<Bouncer />
  			</div>
 		);
 	}	
 });
-
 module.exports = App;
-
-			// <h1 style={{color:'blue'}}>xxiiiii hhhhhhhdss</h1>
--- src/goatstone/main.js
+++ src/goatstone/main.js
@@ -2,6 +2,7 @@
 var React = require('react');
 var App = require('./App.js');
 var Spinner = require('./ui/Spinner.js');
+var Bouncer = require('./ui/Bouncer.js');
 
 React.render(<App/>, document.body);
-React.render(<Spinner/>, document.querySelector('div div')); 
+
